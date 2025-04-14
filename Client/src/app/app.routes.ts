4acea44e214d5c3cde1c6346664933df8108b2d6
Justin <j.hebenstreit25@gmail.com>
2025-04-14 01:10:30 -0500

    Reorganized Angular project: moved frontend files to Client/

---
+++ Client/src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+// import { CssCodeComponent } from './components/cssCode/css-code.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // Default route (Home)
+  
+];
