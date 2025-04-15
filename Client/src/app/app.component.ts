dc554572b736f22a4452ab797c6969c5fc6d6f93
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2025-04-14 21:03:07 -0500

    working on prepping to finish adding Navigation and Pages

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavigationComponent } from './components/navigation/navigation.component';
+import { NavigationComponent } from '@components/NavigationUI/navigation/navigation.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
